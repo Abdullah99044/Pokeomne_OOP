9e40be33e4b3f3e4f398a41bf29a61a770578396
Abdullah99044 <89779550+Abdullah99044@users.noreply.github.com>
2023-08-21 22:21:22 +0200

    Assigment 1

---
+++ Pokemone/Charmendar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP
+{
+    internal class Charmander
+    {
+        //Fields
+
+        public string name;
+        public string strength;
+        public string weakness;
+
+        //Constructor
+        public Charmander(string name, string strength, string weakness)
+        {
+
+            this.name = name;
+            this.strength = strength;
+            this.weakness = weakness;
+
+        }
+
+
+        //Methods
+
+        public void battleCry()
+        {
+
+            Console.WriteLine($"{this.name}!!!!");
+        }
+
+    }
+}
---
+++ Pokemone/Program.cs
@@ -0,0 +1,66 @@
+﻿using APP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace APP
+{
+    internal class Program
+
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            //Player starts his game
+
+            Console.WriteLine("Press a button to start your game");
+            Console.ReadLine();
+
+            while (true)
+            {
+
+
+                //Charmendar name
+
+                Console.WriteLine("Give your Charmendar a name : ");
+                string name = Console.ReadLine();
+
+                Charmander charmander = new Charmander(name, "Fire", "Water");
+
+
+                //Charmendar battle cry
+
+                for (int i = 0; i < 10; i++)
+                {
+                    charmander.battleCry();
+                }
+
+                //Do the player wants to continue ?
+
+                Console.WriteLine("Do you want to continue ? yes/no ");
+
+                string answer = Console.ReadLine();
+
+                if (answer == "yes")
+                {
+
+                    break;
+
+                }
+
+
+            }
+
+
+
+        }
+    }
+}
